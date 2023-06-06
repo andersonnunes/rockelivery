4ca6c010684a22ab7c06c7f4a1601f3cf04f5e3f
Anderson Nunes <anderson.nunes1988@gmail.com>
2023-06-06 11:15:02 -0300

    Tratando erros com o fallback controller

--- lib/rockelivery_web/controllers/error_json.ex
+++ lib/rockelivery_web/controllers/error_json.ex
@@ -1,4 +1,7 @@
 defmodule RockeliveryWeb.ErrorJSON do
+  alias Ecto.Changeset
+
+  import Ecto.Changeset, only: [traverse_errors: 2]
   # If you want to customize a particular status code,
   # you may add your own clauses, such as:
   #
@@ -9,7 +12,19 @@ defmodule RockeliveryWeb.ErrorJSON do
   # By default, Phoenix returns the status message from
   # the template name. For example, "404.json" becomes
   # "Not Found".
-  def render(template, _assigns) do
+  def render(template, _assigns) when template != "400.json" do
     %{errors: %{detail: Phoenix.Controller.status_message_from_template(template)}}
+  end
+
+  def render("400.json", %{result: %Changeset{} = changeset}) do
+    %{message: translate_errors(changeset)}
+  end
+
+  defp translate_errors(changeset) do
+    traverse_errors(changeset, fn {msg, opts} ->
+      Enum.reduce(opts, msg, fn {key, value}, acc ->
+        String.replace(acc, "%{#{key}}", to_string(value))
+      end)
+    end)
   end
 end
