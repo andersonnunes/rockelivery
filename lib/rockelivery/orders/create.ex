9d85c2b6084abeb2be8d1a4cc074ee3b80785d31
Anderson Nunes <anderson.nunes1988@gmail.com>
2023-06-07 20:50:00 -0300

    Refatorando o módulo de criação de Pedido

--- lib/rockelivery/orders/create.ex
+++ lib/rockelivery/orders/create.ex
@@ -1,43 +1,21 @@
 defmodule Rockelivery.Orders.Create do
   import Ecto.Query
+
   alias Rockelivery.{Error, Item, Order, Repo}
+  alias Rockelivery.Orders.ValidateAndMultiplyItems
 
-  def call(params) do
-    params
-    |> fetch_items()
-    |> handle_items(params)
-  end
-
-  defp fetch_items(%{"items" => items_params}) do
+  def call(%{"items" => items_params} = params) do
     items_ids = Enum.map(items_params, fn item -> item["id"] end)
 
-    query = from item in Item, where: item.id in ^items_ids
+    query = from(item in Item, where: item.id in ^items_ids)
 
     query
     |> Repo.all()
-    |> validate_and_multiply_items(items_ids)
+    |> ValidateAndMultiplyItems.call(items_ids, items_params)
+    |> handle_items(params)
   end
 
-  defp validate_and_multiply_items(items, items_ids) do
-    items_map = Map.new(items, fn item -> {item.id, item} end)
-
-    items_ids
-    |> Enum.map(fn id -> {id, Map.get(items_map, id)} end)
-    |> Enum.any?(fn {_id, value} -> is_nil(value) end)
-    |> multiply_items(items_map, items_map)
-  end
-
-  defp multiply_items(true, _items, _item_params), do: {:error, "Invalid ids!"}
-
-  defp multiply_items(false, items, item_params) do
-    items =
-      Enum.reduce(item_params, [], fn %{"id" => id, "quantity" => quantity}, acc ->
-        item = Map.get(items, id)
-        acc ++ List.duplicate(item, quantity)
-      end)
-
-    {:ok, items}
-  end
+  defp handle_items({:error, result}, _params), do: {:error, Error.build(:bad_request, result)}
 
   defp handle_items({:ok, items}, params) do
     params
@@ -45,8 +23,6 @@ defmodule Rockelivery.Orders.Create do
     |> Repo.insert()
     |> handle_insert()
   end
-
-  defp handle_items({:error, result}, _params), do: {:error, Error.build(:bad_request, result)}
 
   defp handle_insert({:ok, %Order{}} = order), do: order
   defp handle_insert({:error, result}), do: {:error, Error.build(:bad_request, result)}
